074515cae6efa3c09154ad06089f5a07f9ea462d
Charlydk <89538903+Charlydk@users.noreply.github.com>
2022-05-05 09:51:50 -0300

    agregar nock de tareas y el interface de las mismas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import{Task} from "./Task"
+export const TASKS =[
+    {
+        id: 1,
+        Text: "terminar primer modulo de angular",
+        day:'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        Text: "Hacer las compras para la cena",
+        day:'Agosto 5 a las 17:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        Text: "investigar sobre boostrap",
+        day:'Agosto 5 a las 19:00',
+        reminder: true
+    },
+    {
+        id: 4,
+        Text: "Leer el libro favorito",
+        day:'Agosto 5 a las 21:00',
+        reminder: true
+    }
+]
